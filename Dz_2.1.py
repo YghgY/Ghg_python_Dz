f45e8d8c70acb276bed9dc7a7de44b04b8c86588
YghgY <YghgYx@gmail.com>
2024-11-08 16:49:48 +0200

    Dz_2.1

---
+++ Dz_2.1.py
@@ -0,0 +1,17 @@
+
+print("Please enter four number:")
+Numbers =input()
+
+num1 = str(Numbers)[0]
+num1 = int(num1) // 1
+num2 = str(Numbers)[1]
+num2 = int(num2) // 1
+num3 = str(Numbers)[2]
+num3 = int(num3) // 1
+num4 = str(Numbers)[3]
+num4 = int(num4) // 1
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
